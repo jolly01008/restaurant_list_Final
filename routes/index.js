9e1ce1cdb4c04c1b98b363eb1a3066976157217a
jolly01008 <jollgmll292@gmail.com>
2023-05-31 21:21:47 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router() //引用express路由器
 
 const home = require('./modules/home') //載入home模組
 const restaurants = require('./modules/restaurants') //載入restaurants模組
+const users = require('./modules/users') // 載入users模組
 
 router.use( '/' , home ) //網址結構符合/字串的request導向home模組
 router.use( '/restaurants' , restaurants ) //網址結構符合/restaurants字串的request導向restaurants模組
+router.use( '/users' , users)
 
 module.exports = router // 匯出路由器
